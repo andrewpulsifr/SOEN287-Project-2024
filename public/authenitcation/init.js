341211ab343b2a5c3406a4a910373319822b4b43
Matthew <matthewpompeo22@gmail.com>
2024-10-30 21:14:32 -0400

    Delete Profile

--- public/authenitcation/init.js
+++ public/authenitcation/init.js
@@ -3,8 +3,14 @@ const auth = new Auth();
 // Ensure this is run only when the .logout button is present
 document.addEventListener('DOMContentLoaded', () => {
     const logoutButton = document.querySelector('.logout');
+    const deleteButton = document.querySelector('.delete-btn');
     if (logoutButton) {
         logoutButton.addEventListener('click', (e) => {
+            auth.logout();
+        });
+    }
+    if (deleteButton) {
+        deleteButton.addEventListener('click', (e) => {
             auth.logout();
         });
     }
